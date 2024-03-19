13caa3b60976d64f884d71a6f8986c15e3e3eb97
Shelton Ngwenya <shelt15.nn@gmail.com>
2024-03-19 16:27:55 +0000

    classroompi set up

--- iaq_sensing.py
+++ iaq_sensing.py
@@ -6,32 +6,49 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 # Configure Logging
+
+# Configure Logging to log to a file
+logging.basicConfig(
+    level=logging.INFO,
+    filename="/home/thepinalyser/.log/iaq_monitoring_script.log",  # Log to this file
+    filemode="a",  # Append to the file, do not overwrite
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 DEVICE_ALTITUDE = 132
-API_KEY = 'eb1be9209cd5488680621512232008'
-LOCATION = 'Cork, Ireland'
+API_KEY = "eb1be9209cd5488680621512232008"
+LOCATION = "Cork, Ireland"
 
 # InfluxDB configurations
-INFLUXDB_TOKEN = "H9kNV8v24yRDULrY3Fkc5hmuKl2S65AIKpCMJwDvPfYEocoeX76n90A4bNVxHBz2MI4QFE9URCvR4agIDIUP2g=="
+INFLUXDB_TOKEN = "iazhNS3IVmiHZljvizszVb38to1FTtK1Vvkv-hBTR_pBQWgfkN76SZmWjbs68SGCrpIBKySCFzupT1vLrUxF6g=="
 INFLUXDB_ORG = "Shelton"
-INFLUXDB_URL = "http://localhost:8086"  
+INFLUXDB_URL = "http://localhost:8086"
 INFLUXDB_BUCKET = "IAQ_Monitoring_Sys"
+
 
 class InfluxDBHandler:
     def __init__(self):
-        self.client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+        self.client = InfluxDBClient(
+            url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG
+        )
 
     def write_to_influxdb(self, co2, temperature, humidity):
         write_api = self.client.write_api(write_options=SYNCHRONOUS)
         # pdb.set_trace()  # Set a breakpoint here
-        point = Point("sensor_data") \
-            .tag("location", LOCATION) \
-            .field("co2", co2.co2) \
-            .field("temperature", temperature.degrees_celsius) \
+        point = (
+            Point("sensor_data")
+            .tag("location", LOCATION)
+            .field("co2", co2.co2)
+            .field("temperature", temperature.degrees_celsius)
             .field("humidity", humidity.percent_rh)
+        )
         write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
+
 
 class SensorHandler:
     def __init__(self, i2c_transceiver):
@@ -47,14 +64,16 @@ class SensorHandler:
     def read_measurement(self):
         return self.scd4x.read_measurement()
 
+
 class WeatherAPIHandler:
     @staticmethod
     def fetch_atmospheric_pressure(api_key, location):
-        url = f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={location}'
+        url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={location}"
         response = requests.get(url)
-        response_data = response.json() if response.status_code == 200 else None        
+        response_data = response.json() if response.status_code == 200 else None
         # pdb.set_trace()  # Set a breakpoint here
-        return response_data.get('current', {}).get('pressure_mb', None)
+        return response_data.get("current", {}).get("pressure_mb", None)
+
 
 class MainApplication:
     def __init__(self):
@@ -62,7 +81,7 @@ class MainApplication:
 
     def run(self):
         try:
-            with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
+            with LinuxI2cTransceiver("/dev/i2c-1") as i2c_transceiver:
                 sensor_handler = SensorHandler(i2c_transceiver)
                 # weather_handler = WeatherAPIHandler()
 
@@ -72,24 +91,25 @@ class MainApplication:
 
                 # est_ambient_pressure = weather_handler.fetch_atmospheric_pressure(API_KEY, LOCATION)
                 # if est_ambient_pressure is not None:
-                    # logger.info(f"Estimated Atmospheric Pressure in {LOCATION} mb: {est_ambient_pressure}")
-                    # sensor_handler.scd4x.set_ambient_pressure(int(est_ambient_pressure))  # Convert to Pa
+                # logger.info(f"Estimated Atmospheric Pressure in {LOCATION} mb: {est_ambient_pressure}")
+                # sensor_handler.scd4x.set_ambient_pressure(int(est_ambient_pressure))  # Convert to Pa
 
                 sensor_handler.start_periodic_measurement()
 
                 while True:
                     time.sleep(60)  # Sleep for 60 seconds (1 minute)
-                    
+
                     co2, temperature, humidity = sensor_handler.read_measurement()
-                    logger.info(f"CO2: {co2}, Temperature: {temperature}, Humidity: {humidity}")
+                    # logger.info(f"CO2: {co2}, Temperature: {temperature}, Humidity: {humidity}")
 
                     # Write data to InfluxDB
                     self.influxdb_handler.write_to_influxdb(co2, temperature, humidity)
 
-
         except Exception as e:
             logger.error(f"An error occurred: {str(e)}")
+
 
 if __name__ == "__main__":
     app = MainApplication()
     app.run()
+
--- start_script.py
+++ start_script.py
@@ -2,24 +2,32 @@
 
 import time
 import subprocess
+import logging
+logging.basicConfig(filename='/home/thepinalyser/.log/start_script.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 def is_container_running(container_name):
     try:
         result = subprocess.run(["docker", "inspect", "-f", "{{.State.Running}}", container_name], capture_output=True, text=True)
+        process = subprocess.run(["/home/thepinalyser/.pyenv/versions/3.9.2/bin/python", "/home/thepinalyser/Documents/Air_Quality_Monitoring_Sys/iaq_sensing.py"])
+        if process.returncode != 0:
+            logging.error(f"iaq_sensing.py exited with return code {process.returncode}")
         return result.stdout.strip() == "true"
     except Exception as e:
-        print(f"Error checking container status: {e}")
+        logging.error(f"Error checking container status: {e}")
         return False
 
 if __name__ == "__main__":
-    container_name = "influxdb"
+    container_name = "influx-influxdb-1"
 
     while True:
         if is_container_running(container_name):
-            print("Starting your Python script...")
+            logging.info("Starting your Python script...")
             # Replace with the actual command to start your Python script
-            subprocess.run(["python3", "/home/shelt/Documents/Code/Air_Quality_Monitoring_Sys/iaq_sensing.py"])
+            subprocess.run(["python3", "/home/thepinalyser/Documents/Air_Quality_Monitoring_Sys/iaq_sensing.py"])
             break  # Exit loop once the script is run
         else:
-            print(f"Waiting for the '{container_name}' container to start...")
+            logging.info(f"Waiting for the '{container_name}' container to start...")
             time.sleep(10)  # Adjust the polling interval as needed
